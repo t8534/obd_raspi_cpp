f07f7775c490a117ba952d5506ba08b98a1b3d29
arek <areklib@poczta.onet.pl>
2025-11-17 20:31:13 +0100

    Added example of file management following RAII rule.

--- src/DataLogger.cpp
+++ src/DataLogger.cpp
@@ -7,6 +7,40 @@
 
 #include "DataLogger.h"
 
+
+// Example of the file management - RAII
+// p.386 Kompendium C++
+//
+/*
+class File_ptr
+{
+
+	FILE* p;
+
+public:
+
+	File_ptr(const char* n. const char* a) : p{fopen(n, a)}  // Open the file
+	{
+		if (p == null_ptr) throw runtime_error{"File_ptr: the file cannot be opened"};
+	}
+
+	File_ptr(const string& n. const char* a) : File_ptr{n. c_str(), a} {}  // Open the file
+
+	explicit File_ptr(FILE* pp) : p{pp}  // Przyjecie zalozenia dot. wlasnosci pp
+	{
+		if (p == null_ptr) throw runtime_error{"File_ptr: nullptr"};
+	}
+
+	// odpowiednie operacje przenoszenia i kopiowania
+
+	~File_ptr() { fclose(p); }
+
+	operator FILE*() { return p; }
+
+};
+*/
+
+
 DataLogger::DataLogger() {
 	// TODO Auto-generated constructor stub
 
